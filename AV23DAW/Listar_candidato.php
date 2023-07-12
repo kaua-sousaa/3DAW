ea3be4b32ee288ea2e43e80b647b2cf738f4c622
Kauã <102885029+kaua-sousaa@users.noreply.github.com>
2023-07-11 23:24:54 -0300

    Versao final

--- AV23DAW/Listar_candidato.php
+++ AV23DAW/Listar_candidato.php
@@ -9,7 +9,7 @@
     die("Erro na conexão com o banco de dados: " . mysqli_connect_error());
     }
 
-    $comandoSQL = "SELECT * FROM candidato ORDER BY nome ASC";
+    $comandoSQL = "SELECT * FROM candidato ORDER BY salaProva ASC, nome ASC";
     $resultado = $mysqli->query($comandoSQL);
 
     $registros = array();
